ada174d0d22899752dd1c44a75d1fdef6e8e5773
cam4555 <cam4555@rit.edu>
2020-08-17 22:38:35 -0400

    redisgn with selenium

--- serv.py
+++ serv.py
@@ -1,27 +1,27 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from subprocess import check_output
-
-import urllib.parse
-import webbrowser
 import os
+import urllib.parse
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-
-def get_pid(name):
-    return check_output(["pidof",name])
 
 class Serv(BaseHTTPRequestHandler):
     def do_GET(self):
 
+
+        driver = webdriver.Chrome(executable_path='/home/chase/.local/bin/chromedriver')
+
         if self.path == '/':
             self.path = '/index.html'
         
-        if not(os.path.isfile(self.path[1:])):
+        if  (self.path.find("index") == -1 and self.path.find("style") == -1):
             link = self.path[14::]
             link = urllib.parse.unquote(link)
-            os.kill(pid, get_pid("chromium-browser"))
-            webbrowser.open(link)
+            driver.get(link)
             self.path = 'index.html'
-            
+              
         try: 
             file_to_open = open(self.path[1:]).read()
             self.send_response(200)
@@ -32,6 +32,9 @@ class Serv(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
+        print ("\n \n \n \n ")
+        print (self.path)
+        print ("\n \n \n \n ")
 
 httpd = HTTPServer(('0.0.0.0', 8080), Serv)
 httpd.serve_forever()
