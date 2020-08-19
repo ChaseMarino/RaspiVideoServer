fbba4e21cb62b0e6f6dcd79fbca5c83491e3f3f1
cam4555 <cam4555@rit.edu>
2020-08-18 20:36:11 -0400

    broke git

--- serv.py
+++ serv.py
@@ -27,7 +27,7 @@ class Serv(BaseHTTPRequestHandler):
         content = self.rfile.read(content_length).decode('utf-8')
 
         if self.path == "/page_load":
-            self.page_load(content)
+            self.handle_page_load(content)
         elif self.path == "/fullscreen":
             self.handle_fullscreen(content)
         
